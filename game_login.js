5bdc29577ae97c24c08dd2e78f9d33d7fe459d12
bikeblues <36891729+bikeblues@users.noreply.github.com>
2022-07-05 19:34:41 -0500

    Update game_login.js

--- game_login.js
+++ game_login.js
@@ -7,10 +7,4 @@ function addUser(){
   localStorage.setItem("player2_name","player2_name");
     window.location = "game_page.html";
 }
-  // Get value of user by id player1_name_input and player2_name_input
-
-  // Store these values locally
-
-  //Assign "game_page.html" to window.location
-}
-
+  
